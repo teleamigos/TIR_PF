39a35a9e3485eb766a234c03ea85ed2abb54196b
OrlandoTele96 <orlandoglezmusic@gmail.com>
2019-11-27 00:08:08 -0600

    Now compute MPRs

--- Topology.py
+++ Topology.py
@@ -10,7 +10,31 @@ class Topology:
         self.Neighbors=[]
         self.Topology={}
         self.MyMPR=''
-        self.MPRs={}
+        self.MPRs=[]
 
     def MPR(self):
-        pass
+        aux=0
+        i=0
+        c=0
+        lista=[]
+        vecinos=[]
+        for Node in self.Topology:
+            if Node !='0':
+                L=len(self.Topology[Node])
+                lista.append((Node,L))
+        for nodo in lista:
+            c=0
+            if nodo[1]>1:
+                if len(vecinos)==0:
+                    vecinos.append(self.Topology[nodo[0]])
+                    self.MPRs.append(nodo[0])
+                else:
+                    for v in self.Topology[nodo[0]]:
+                        if v in vecinos[0]:
+                            pass
+                        else :
+                            vecinos[0].append(v)
+                            c+=1
+                    if c>1:
+                        self.MPRs.append(nodo[0])
+        """print(vecinos)"""
