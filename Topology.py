a444d3ef7505267c986df005c25de0dbf2fc8b19
OrlandoTele96 <orlandoglezmusic@gmail.com>
2019-11-28 19:23:28 -0600

    Comtue MPR to the Topology class

--- Topology.py
+++ Topology.py
@@ -3,7 +3,7 @@
 --------------------------------------------------------------------------------
 1.- Init
 -----------------------------------------------------------------------------"""
-
+import time
 class Topology:
     def __init__(self,ID):
         self.MyID=ID
@@ -11,6 +11,33 @@ class Topology:
         self.Topology={}
         self.MyMPR=[]
         self.SOYMPR=0
+        self.around=[]
 
     def MPR(self):
-        pass
+        nei=[]
+        for i in self.Topology:
+            nei.append(i)
+            for j in self.Topology[i]:
+                try:
+                    self.around.index(j)
+                except:
+                    self.around.append(j)
+        self.around.sort()
+        self.around.remove(self.MyID)
+        for i in nei:
+            try:
+                self.around.remove(i)
+            except:
+                time.sleep(0.05)
+        aux=0
+        for k in self.around:
+            for i in self.Topology:
+                for j in self.Topology[i]:
+                    if k == j and aux == 0:
+                        try:
+                            self.MyMPR.index(i)
+                        except:
+                            self.MyMPR.append(i)
+                            aux = 1
+        aux = 0
+        print(self.MyMPR)
